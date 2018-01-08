f4cde68d4bd96016b8e6926ec2c723fa2f3ee951
Gustavo F. Silva <gustavofsilva@bitteam.com.br>
2018-01-08 02:49:45 -0200

    Adjusting settings.

--- amazon/settings.py
+++ amazon/settings.py
@@ -21,6 +21,8 @@ NEWSPIDER_MODULE = 'amazon.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+DOWNLOAD_DELAY = 0.5
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
 
